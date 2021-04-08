bce1741e891e168b97eaa758937a2e925fafe7a6
Nayan <nayan.pradhan@hotmail.com>
2021-04-08 17:36:12 +0200

    Adding seperate py file for training

--- train_model.py
+++ train_model.py
@@ -9,4 +9,44 @@ import spacy
 from sklearn.svm import LinearSVC
 import time
 import pickle 
+import os
 
+data = pd.read_csv('amazon_data.txt', sep='\t', header=None)
+data.columns = ["Review", "Sentiment"]
+
+punctuation = string.punctuation
+stop_words = list(STOP_WORDS)
+nlp = spacy.load("en_core_web_sm")
+numbers = string.digits
+
+## function that cleans input text
+def cleaning_function(input_text):
+    text = nlp(input_text)
+    tokens = []
+    for token in text:
+        temp = token.lemma_.lower()
+        tokens.append(temp)
+    cleaned_tokens = []
+    for token in tokens:
+        if token not in stop_words and token not in punctuation and token not in numbers:
+            cleaned_tokens.append(token)
+    return cleaned_tokens
+
+X = data["Review"]
+y = data["Sentiment"]
+
+## SVC using tfidf (bag of words)
+tfidf = TfidfVectorizer(tokenizer = cleaning_function)
+classifier = LinearSVC()
+SVC_clf = Pipeline([('tfidf', tfidf), ('clf', classifier)])
+print("Training Model...")
+SVC_clf.fit(X, y)
+print("Model Trained!")
+
+fname = 'saved_model.sav'
+# os.mkfifo(fname)
+print("Saving Model...")
+with open(fname, 'wb') as f:
+    pickle.dump(SVC_clf, f)
+    
+print("Model Saved")
